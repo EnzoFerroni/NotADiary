81b97b2f6786fa48a4eb4bc0b015ca301748634a
Pedro Augusto <tesssaro@10-44-48-19.0200.mack.br.iosda.org>
2025-10-06 13:56:40 -0300

    Initial Commit

---
+++ CrazyDiary/CrazyDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrazyDiaryApp.swift
+//  CrazyDiary
+//
+//  Created by Pedro Augusto on 06/10/25.
+//
+
+import SwiftUI
+
+@main
+struct CrazyDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
