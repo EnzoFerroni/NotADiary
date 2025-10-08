0089d3896d43aaa0ee6c4d5e661f4029560cce89
Pedro Augusto <tesssaro@Tessaros-Macbook.local>
2025-10-08 14:27:59 -0300

    feat: changes before all screen cover

--- NotADiary/Views/EntryView.swift
+++ NotADiary/Views/EntryView.swift
@@ -9,34 +9,68 @@ import SwiftUI
 import CloudKit
 
 struct EntryView: View {
+    @Environment(\.dismiss) var dismiss
+    
     @StateObject var viewModel = EntryViewModel()
     
     @State private var ckViewModel = CloudKitViewModel()
     
+    @State private var isLoading: Bool = true
+    
     var body: some View {
-        if ckViewModel.isLogged {
-            VStack {
-                Text("Oi, \(ckViewModel.preference?.name ?? "") você está logado!!!!")
-            }
-        } else {
-            Form {
-                Text("opa, parece que você não está logado")
-                    .font(.title2)
-                
-                LabeledContent {
-                    TextField("", text: $ckViewModel.name)
-                } label: {
-                    Text("Digite seu nome:")
-                }
-                
-                Button {
-                    if !ckViewModel.name.isEmpty {
-                        ckViewModel.loginButtonPressed()
+        if isLoading {
+            ProgressView()
+                .progressViewStyle(CircularProgressViewStyle(tint: .blue))
+                .scaleEffect(2.0, anchor: .center)
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                        isLoading = false
                     }
-                } label: {
-                    Text("Cadastrar")
                 }
-                .disabled(viewModel.isSignedInToiCloud.description.uppercased() == "TRUE" ? false : true)
+        } else {
+            NavigationStack {
+                if ckViewModel.isLogged {
+                    VStack {
+                        Text("Oi, \(ckViewModel.preference?.name ?? "") você está logado!!!!")
+                            .font(.title)
+                        Button {
+                            
+                        } label: {
+                            Text("Entrar")
+                        }
+                    }
+                }
+                else {
+                    if viewModel.isSignedInToiCloud.description.uppercased() != "FALSE" {
+                        Form {
+                            Text("Opa, parece que você não está logado")
+                                .font(.title)
+                            
+                            LabeledContent {
+                                TextField("", text: $ckViewModel.name)
+                            } label: {
+                                Text("Digite seu nome:")
+                            }
+                            
+                            Button {
+                                if !ckViewModel.name.isEmpty {
+                                    ckViewModel.loginButtonPressed()
+                                }
+                            } label: {
+                                Text("Cadastrar")
+                            }
+                            .buttonStyle(.borderedProminent)
+                        }
+                    }
+                    else {
+                        VStack {
+                            Text("Infelizmente você não está logado em uma conta do iCloud")
+                                .font(.title)
+                            Text("Vá para as configurações do celular, clique em iCloud e faça seu login!")
+                                .font(.title2)
+                        }
+                    }
+                }
             }
         }
     }
